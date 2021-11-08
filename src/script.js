4f0119696df11dca334c2f075b1ba7b3b0d0c8e1
Charlie-Beth <c.thompson1990@hotmail.co.uk>
2021-11-08 17:16:23 +0000

    Updated project files

---
+++ src/script.js
@@ -0,0 +1,56 @@
+
+
+function searchTempertature( response){
+   let temperature = Math.round(response.data.main.temp);
+   document.querySelector("#temp").innerHTML = (Math.round(response.data.main.temp));
+   document.querySelector("#city").innerHTML = response.data.name;
+  }
+
+  function searchCity(city){
+    axios
+      .get(`${apiUrl}${city}&appid=${apiKey}&units=${units}`).then(searchTempertature);
+  }
+
+
+function getCity(event){
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+let apiKey = "16a7878dc2b4a449ac0b491099d29fe3";
+let units = "metric";
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+
+let search = document.querySelector("#search");
+search.addEventListener("submit", getCity);
+
+searchCity("London");
+
+function showTemperature (response){
+   document.querySelector("#temp").innerHTML = (Math.round(response.data.main.temp));
+   document.querySelector("#city").innerHTML = response.data.name;
+}
+
+function getLocation(position){
+   axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`
+   ).then(showTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
